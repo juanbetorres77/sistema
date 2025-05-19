425b8d30399dd3cab5bf9c4b5b5b83d1933ee8d2
juanbetorres77 <juanbetorres@hotmail.com>
2025-05-19 17:42:32 -0300

    Add files via upload

---
+++ js/funcionesSistema.js
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------------------------
+function cerrarSesion() {
+    alertify.confirm("Nombre del sistema", "¿Quieres cerrar la sesión del usuario?",
+        function(){
+            localStorage.removeItem("nomUsuario"); //Elimina el nombre del usuario
+            window.location.href = "login.html"; // Redirige al login
+        },
+        function(){
+            
+        }
+    ).set('labels', {ok:'Sí', cancel:'No'}).set('transition', 'slide');
+}
+//---------------------------------------------------------------------------------------
